3923b3b6339023a52d119e48f37439e45e7897d9
LegoFigure11 <24732684+LegoFigure11@users.noreply.github.com>
2025-11-02 10:46:04 +1100

    Add alpha support for stashed shinies

--- Furfrou.WinForms/MainWindow.cs
+++ Furfrou.WinForms/MainWindow.cs
@@ -564,8 +564,9 @@ public partial class MainWindow : Form
                             1 => " (F)",
                             _ => "",
                         };
+                        var alpha = pk is IAlpha { IsAlpha: true } ? "-α" : string.Empty;
                         s +=
-                            $"{Strings.Species[pk.Species]}{gender} - {Strings.Natures[(int)pk.Nature]} - {pk.IV_HP:D2}/{pk.IV_ATK:D2}/{pk.IV_DEF:D2}/{pk.IV_SPA:D2}/{pk.IV_SPD:D2}/{pk.IV_SPE:D2}\n";
+                            $"{Strings.Species[pk.Species]}{alpha}{gender} - {Strings.Natures[(int)pk.Nature]} - {pk.IV_HP:D2}/{pk.IV_ATK:D2}/{pk.IV_DEF:D2}/{pk.IV_SPA:D2}/{pk.IV_SPD:D2}/{pk.IV_SPE:D2}\n";
                     }
 
                     MessageBox.Show(s);
